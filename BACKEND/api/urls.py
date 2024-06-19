081020cdf61c54ea15547eb90e6a2fa6c65ee7d4
john olanipekun <jolanipekun23@gmail.com>
2024-06-19 22:29:31 +0100

    first commit

---
+++ BACKEND/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register('project', ProjectViewSet, basename='project')
+urlpatterns = router.urls
+
+'''
+urlpatterns = [
+
+    path('', home)
+]
+'''
